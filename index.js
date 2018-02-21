6e00460c9d93ea463edf232963fabbe5f5bd201a
Christopher Moser <moser121@icloud.com>
2018-02-21 16:28:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function HappyHolidaysTo(name){
 }
 
 function happyHolidayTo(holiday, name){
-  return `Happy ${holiday},${name}!`
+  return `Happy ${holiday}, ${name}!`
 }
 
 function holidayCountdown(holiday, days){
