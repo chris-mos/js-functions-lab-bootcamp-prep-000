ac36668db6bb1c6cdd08cd4be7af5672ea03e1a1
Christopher Moser <moser121@icloud.com>
2018-02-21 16:23:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ return "Happy holidays!"
 
 function HappyHolidaysTo(name){
   return `Happy holidays, ${name}!`;
-};
+}
 
 function happyHolidayTo(holiday, name){
   return `Happy ${holiday},${name}!`
